c50c4f7f746a68ba99d79b85e8e89cda2ebed489
lucasanadao <lucasaugusto565@gmail.com>
2024-05-13 18:59:33 -0300

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,22 +5,63 @@
 
 `default_nettype none
 
-// just a stub to keep the Tiny Tapeout tools happy
-
 module tt_um_teste_tinytapeout (
-    input  wire [7:0] ui_in,
-    output wire [7:0] uo_out,
-    input  wire [7:0] uio_in,
-    output wire [7:0] uio_out,
-    output wire [7:0] uio_oe,
-    input  wire       ena,
-    input  wire       clk,
-    input  wire       rst_n
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // will go high when the design is enabled
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+reg [7:0] ui_in_reg, uio_in_reg;
+reg [7:0] uo_out_reg, uio_out_reg;
+reg [7:0] uo_out_reg_next, uio_out_reg_next;
+
+reg [7:0] uio_oe_reg;
+
+always @(posedge clk or negedge rst_n) begin
+    if (~rst_n) begin
+        ui_in_reg <= 8'b0;
+        uio_in_reg <= 8'b0;
+    end
+    else begin
+        ui_in_reg <= ui_in;
+        uio_in_reg <= uio_in;
+    end
+end
+
+always @(posedge clk or negedge rst_n) begin
+    if (~rst_n) begin
+        uo_out_reg <= 8'b0;
+        uio_out_reg <= 8'b0;
+    end
+    else begin
+        uo_out_reg <= uo_out_reg_next;
+        uio_out_reg <= uio_out_reg_next;
+    end
+end
+
+always @* begin
+    // By default, outputs are 0
+    uo_out_reg_next = 8'b0;
+    uio_out_reg_next = 8'b0;
+    uio_oe_reg = 8'b0;
+
+    // If enabled, sum inputs and set all outputs to 0
+    if (ena) begin
+        // Example: sum the first 4 bits of ui_in
+        uo_out_reg_next = ui_in_reg[3:0] + ui_in_reg[7:4];
+        uio_out_reg_next = 8'b0; // All outputs are 0
+        uio_oe_reg = 8'b11111111; // All bits high to enable output path
+    end
+end
+
+// Assign outputs
+assign uo_out = uo_out_reg;
+assign uio_out = uio_out_reg;
+assign uio_oe = uio_oe_reg;
 
 endmodule
